787877f83894b27e72916afb781c731994660e3c
cotyembry <cotyembry@gmail.com>
2017-03-24 14:29:11 -0500

    Added more to file to have the bundle.js file moved after it is built

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+const WebpackShellPlugin = require('webpack-shell-plugin');
+//Current build path represents where the file needs to go to
+var MOVE_FILE_TO = 'Y:/marsjs/js/build/distribute';
+var RECOMPILE_ON_THIS_FILE = MOVE_FILE_TO + '/bundle.js';
+var shellCommand2 = 'mv ./dist/bundle.js ' + MOVE_FILE_TO;
+var _onBuildEnd = shellCommand2;
+
 module.exports = {
   entry: './js/index.js',
   output: {
@@ -28,25 +35,9 @@ module.exports = {
             }
         }
     ]
-  }
+  },
+  plugins: [
+    // new WebpackShellPlugin({onBuildStart:['echo "Webpack Start"'], onBuildEnd:['echo "Webpack End"']})
+    new WebpackShellPlugin({onBuildStart:['echo "Webpack Start"'], onBuildEnd:[_onBuildEnd]})
+  ]
 }
-
-
-// module.exports = {
-// 	entry: './js/svgBarGraph.js',
-// 	output: {
-// 		path: __dirname
-// 		filename: './dist/bundle.js',
-// 	},
-// 	module: {
-// 	    loaders: [{
-// 	    	test: /\.jsx?$/,
-// 	    	exclude: /node_modules/,
-// 	    	loader: 'babel-loader',
-// 	    	query: {
-// 	        	plugins: ['transform-runtime'],
-// 	        	presets: ['es2015', 'stage-0', 'react'],
-// 	      	}
-// 	    }]
-//   	}
-// }
