9c6448de2e05511bfcd38f45bacd2a5adc460757
Coty Embry <Coty Embry>
2017-04-14 15:08:33 -0500

    this is in a great place to be a boilerplate specifically for SVG Icon building - open up App.jsx and get to work ^_^

--- js/components/App.jsx
+++ js/components/App.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
 
-import Parent from './Parent.jsx';
+// import blah as SVG from 'react-svg';
+
+// import Svg,{
+//     Circle,
+//     Ellipse,
+//     G,
+//     LinearGradient,
+//     RadialGradient,
+//     Line,
+//     Path,
+//     Polygon,
+//     Polyline,
+//     Rect,
+//     Symbol,
+//     Text,
+//     Use,
+//     Defs,
+//     Stop
+// } from 'react-native-svg';
+
+// import Parent from './Parent.jsx';
+// <Parent numberOfChildren={7} />
 
 export default class App extends React.Component {
 	render() {
 		return (
-			<div>
-				<Parent numberOfChildren={7} />
-			</div>
+			<svg width='200' height='200'>
+                <path d={
+                		//this shape 
+                		//the first two are for the rewind button
+                		'M 25 100 L 49 88 L 49 112' +
+                        'M 49 100 L 73 88 L 73 112' +
+                        //this next one is for the play button (starts at its top left corner)
+                        'M 88 88 L 112 100 L 88 112' +
+                        //the next two will be for the fast-forward button (starts at its top left corner)
+                        'M 127 88 L 151 100 L 127 112' +
+                        'M 151 88 L 175 100 L 151 112'
+                        }
+
+
+                	stroke="black" />
+            </svg>
 		)
 	}
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,4 @@
-/* uncomment the following block and the respective line within the plugins section for the webpack config to get this working
-const WebpackShellPlugin = require('webpack-shell-plugin');
-//Current build path represents where the file needs to go to
-var MOVE_FILE_TO = 'Y:/marsjs/js/build/distribute';
-var RECOMPILE_ON_THIS_FILE = MOVE_FILE_TO + '/bundle.js';
-var shellCommand2 = 'mv ./dist/bundle.js ' + MOVE_FILE_TO;
-var _onBuildEnd = shellCommand2;
-*/
+
 
 module.exports = {
   entry: './js/index.js',
